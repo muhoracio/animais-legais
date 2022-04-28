dc2c9db078bba735e27ca5c81cbda76e7ff07877
Murilo Horácio <muhoracio@gmail.com>
2022-04-28 11:03:38 -0300

    ScrollAnima refatorado em Class

---
+++ js/modules/scroll-anima.js
@@ -0,0 +1,27 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowAltura = window.innerHeight * (60 / 100);
+
+    this.animaScroll = this.animaScroll.bind(this);
+  }
+
+  animaScroll() {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - this.windowAltura < 0;
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else if (section.classList.contains("ativo")) {
+        section.classList.remove("ativo");
+      }
+    });
+  }
+
+  init() {
+    if (this.sections.length) {
+      this.animaScroll();
+      window.addEventListener("scroll", this.animaScroll);
+    }
+  }
+}
