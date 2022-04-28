7da004627fc827999156313e0d8466efec73ded9
Murilo Horácio <muhoracio@gmail.com>
2022-04-28 12:00:09 -0300

    ScrollAnima refeito

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,27 +1,46 @@
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.windowAltura = window.innerHeight * (60 / 100);
+    this.alturaDeVerificacao = window.innerHeight * (60 / 100);
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistances = this.checkDistances.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.windowAltura < 0;
-      if (isSectionVisible) {
-        section.classList.add("ativo");
-      } else if (section.classList.contains("ativo")) {
-        section.classList.remove("ativo");
+  // Pega a distância de cada item
+  // em relação ao topo do site
+  getDistances() {
+    this.distances = [...this.sections].map(section => {
+      const offsetY = section.offsetTop;
+      return {
+        element: section,
+        offsetY: Math.floor(offsetY - this.alturaDeVerificacao)
+      };
+    });
+  }
+
+  // Vefifica a distância de cada elemento 
+  // em relação ao scroll do site
+  checkDistances() {
+    const pageScroll = window.pageYOffset;
+    this.distances.forEach(i => {
+      if (pageScroll > i.offsetY) {
+        i.element.classList.add("ativo");
+      } else if (i.element.classList.contains("ativo")) {
+        i.element.classList.remove("ativo");
       }
     });
   }
 
   init() {
     if (this.sections.length) {
-      this.animaScroll();
-      window.addEventListener("scroll", this.animaScroll);
+      this.getDistances();
+      this.checkDistances();
+      window.addEventListener("scroll", this.checkDistances);
     }
+  }
+
+  // Remove o Event de Scroll
+  stop() {
+    window.removeEventListener("scroll", this.checkDistances);
   }
 }
