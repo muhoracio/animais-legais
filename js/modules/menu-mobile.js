ad3c168729db93715d33291910dfc3f6db4bd741
Murilo Horácio <muhoracio@gmail.com>
2022-05-10 12:59:17 -0300

    Correções gerais de Bugs

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,28 +1,29 @@
 import outsideClick from "./outsideclick.js";
 
 export default class MenuMobile {
-  constructor(btn, list) {
+  constructor(btn, list, events) {
     this.menuButton = document.querySelector(btn);
     this.menuList = document.querySelector(list); 
 
-    // Seleciona o evento com base no tamanho da tela
-    this.event = (window.matchMedia("max-width: 700px").matches) ? "touchstart" : "click";
-    // this.events = (events === undefined) ? ['touchstart', 'click'] : events;
+    this.events = events ?? ['touchstart', 'click'];
 
     this.openMenu = this.openMenu.bind(this);
   }
 
-  openMenu() {
+  openMenu(e) {
+    e.preventDefault();
     this.menuList.classList.add("active");
     this.menuButton.classList.add("active");
-    outsideClick(this.menuList, [this.event], () => {
+    outsideClick(this.menuList, this.events, () => {
       this.menuList.classList.remove("active");
       this.menuButton.classList.remove("active");
     });
   }
 
   addMenuButtonEvents() {
-    this.menuButton.addEventListener(this.event, this.openMenu);
+    this.events.forEach((event) => {
+      this.menuButton.addEventListener(event, this.openMenu);
+    })
   }
 
   init() {
